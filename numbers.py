287ad68ed834fd32775c1a4b2f5de4b9fc845228
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-01 16:17:11 +0200

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,17 @@
+number1 = int(input("Enter 1st number:")) # placed int fuction before input to make into integer
+print(number1)
+number2 = int(input("Enter 2nd number:"))
+print(number2)
+number3 = int(input("Enter 3rd number:"))
+print(number3)
+
+sum_of_all_numbers = number1 + number2 + number3
+print(sum_of_all_numbers)
+minus = number1 - number2
+print(minus)
+multiply = number3 * number1
+print(multiply)
+sum_of_all_numbers = number1 + number2 + number3 
+print(sum_of_all_numbers)
+spam = sum_of_all_numbers / number3
+print(spam)
